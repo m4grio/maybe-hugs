1e9d0e98c9d99f175331885c0f8d6ec89091c6dd
Mario Álvarez <ahoy@m4grio.me>
2017-08-29 23:47:26 +0800

    Using pattern matching

--- elixir/cutie.ex
+++ elixir/cutie.ex
@@ -1,12 +1,13 @@
 defmodule Cutie do
 
   @spec hug(%{}) :: String
-  def hug(cutie) do
-    if cutie[:accepts] do
-      cutie[:name] <> " accepts hug!"
-    else
-      cutie[:name] <> " prefers empathy!"
-    end
+
+  def hug(%{:name => name, :accepts => :true}) do
+    "#{name} accepts hug!"
+  end
+
+  def hug(%{:name => name, :accepts => :false}) do
+    "#{name} prefers empathy!"
   end
 
 end
--- elixir/maybe_hug.ex
+++ elixir/maybe_hug.ex
@@ -1,8 +1,14 @@
 import IO
 require Cutie
 
-cutie_person = %{:name => "John",:accepts => true}
-empathic_person = %{:name => "Mary",:accepts => false}
+cutie_person = %{
+  :name => "John",
+  :accepts => true,
+}
+empathic_person = %{
+  :name => "Mary",
+  :accepts => false,
+}
 
 puts Cutie.hug(cutie_person)
 puts Cutie.hug(empathic_person)
